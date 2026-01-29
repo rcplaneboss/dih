34de80b655e9d74206abad8e627bab7bdb8f1b93
rcplaneboss <qosimabdulrahman0@gmail.com>
2026-01-29 07:49:07 +0100

    fourth commit

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,7 @@ const limiter = rateLimit({
 app.use(limiter);
 app.use(cors({
   origin: ['https://dih-iryq.vercel.app', 'http://localhost:3000', 'http://127.0.0.1:5500', 'http://localhost:5500'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
 app.use(express.json());
